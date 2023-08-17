8e6c75212e5450692841d105e8cf8b1675669c9b
Brainstorma ⚔️ <113864294+Brainstorma@users.noreply.github.com>
2023-08-17 01:02:58 +0000

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,33 @@
-#!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
 import requests
 from functools import wraps
-from typing import Callable
+import redis
 
+redis = redis.Redis()
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+def count_url_access(url):
+    key = f'count:{url}'
+    redis.incr(key)
+    redis.expire(key, 10)
 
+def cache_url(url):
+    key = f'cache:{url}'
+    if redis.exists(key):
+        return redis.get(key)
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    html = requests.get(url).text
+    redis.setex(key, 10, html)
+    return html
 
+def get_page(url):
+    count_url_access(url)
+    return cache_url(url)
 
-@data_cacher
-def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+@app.route('/')
+def index():
+    return get_page('http://slowwly.robertomurray.co.uk')
+
+@app.route('/count')
+def count():
+    url = 'http://slowwly.robertomurray.co.uk'
+    count = int(redis.get(f'count:{url}'))
+    return str(count)
